8f0eb202357c74a0ab9c1750219cede5e5879fa6
ColetteIsabella <coletteisabella@gmail.com>
2013-09-11 17:52:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nesting::Application.config.secret_key_base = '1882554557a61da655d3a5029a28c2746a1d8109851f52342dfca6234825ff73e352b7367afd17fb86128d978464c36274c7318ce645e3bdf943d5a1b102f5aa'
